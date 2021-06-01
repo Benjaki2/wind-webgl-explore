7a5259864a22d50a3d0012175e23bf900b5d1dbb
Ben <benjamin.a.king@nasa.gov>
2021-06-01 11:48:37 -0400

    WIP

--- src/index.js
+++ src/index.js
@@ -6,11 +6,41 @@ import reportWebVitals from './reportWebVitals';
 import { vectorData, extent } from './data';
 import kriging from "@sakitam-gis/kriging";
 import { convert, encode } from './util';
-
+import WindGL from './wind/index';
+const width = 512;
+const height = 512;
 var canvas = document.createElement('canvas');
+var glCanvas = document.createElement('canvas'); // eslint-disable-line
+
+const pxRatio = Math.max(Math.floor(window.devicePixelRatio) || 1, 2);
+glCanvas.width = width;
+glCanvas.height = height;
+const gl = glCanvas.getContext('webgl', {antialiasing: false});
+
+gl.width =width;
+gl.height = height;
+
+const wind = window.wind = new WindGL(gl);
+wind.numParticles = 262144;
+function frame() {
+  if (wind.windData) {
+      wind.draw();
+  }
+  requestAnimationFrame(frame);
+}
+frame();
+updateRetina();
+
+function updateRetina() {
+    const ratio = wind.meta['retina resolution'] ? pxRatio : 1;
+    canvas.width = glCanvas.clientWidth * ratio;
+    canvas.height = glCanvas.clientHeight * ratio;
+    wind.resize();
+}
 var ctx = canvas.getContext('2d');
-canvas.width  = 512;
-canvas.height = 512;
+
+canvas.width  = width;
+canvas.height = height;
 const NUM_POINTS = vectorData.length;
 const vy = new Float32Array(NUM_POINTS),
     x = new Float32Array(NUM_POINTS),
@@ -34,28 +64,47 @@ const vxScale = vxMax - vxMin;
 const vyScale = vyMax - vyMin;
 const variogram_x = kriging.train(vx, x, y, "exponential", 0, 10);
 const variogram_y = kriging.train(vy, x, y, "exponential", 0, 10);
-for (let xx = 0; xx <= 512; ++xx) {
-  for (let yy = 0; yy <= 512; ++yy) {
-    var vxpredicted = kriging.predict(xx, yy, variogram_x);
-    var vypredicted = kriging.predict(xx, yy, variogram_y);
-    ctx.putImageData(new ImageData(new Uint8ClampedArray([encode(xx, 512), encode(yy, 512), encode(vxpredicted, vxScale), encode(vypredicted, vyScale)]),1 ,1), xx, yy);
+
+
+
+// const png = new PNG({
+//   colorType: 2,
+//   filterType: 4,
+//   width: width,
+//   height: height
+// });
+
+for (let y = 0; y < height; y++) {
+  for (let x = 0; x < width; x++) {
+      var vxpredicted = kriging.predict(x, y, variogram_x);
+      var vypredicted = kriging.predict(x, y, variogram_y);
+      const r = Math.floor(255 * (x - 0) / (512));
+      const g = Math.floor(255 * (vxpredicted - vxMin) / (vxMax - vxMin));
+      const b = Math.floor(255 * (y - 0) / (512));
+      const a = Math.floor(255 * (vypredicted - vyMin) / (vyMax - vyMin));
+      ctx.putImageData(new ImageData(new Uint8ClampedArray([r, g, b, a]), 1 ,1), x, y);
   }
 }
 const root = document.getElementById('root');
 var dataURL = canvas.toDataURL();
-
-// root.appendChild(canvas);
-
+wind.setWind(canvas.toDataURL());
 
 
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App dataURL={dataURL} />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+root.appendChild(glCanvas);
+
+
+
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App dataURL={dataURL} />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
