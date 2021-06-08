4b1d1ff7ddedfd6e262d5fc794c16981e516b68d
Ben <benjamin.a.king@nasa.gov>
2021-06-08 12:03:51 -0400

    tested with png img as data source

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { vectorData, extent } from './data';
 import kriging from "@sakitam-gis/kriging";
+import { convert, encode } from './util';
 import WindGL from './wind/index';
 const width = 512;
 const height = 512;
+var canvas = document.createElement('canvas');
 var glCanvas = document.createElement('canvas'); // eslint-disable-line
 const meta = {};
 const pxRatio = Math.max(Math.floor(window.devicePixelRatio) || 1, 2);
@@ -19,7 +23,7 @@ gl.width =width;
 gl.height = height;
 
 const wind = window.wind = new WindGL(gl);
-wind.numParticles = 15000;
+wind.numParticles = 11024;
 function frame() {
   if (wind.windData) {
       wind.draw();
@@ -30,15 +34,26 @@ frame();
 if (pxRatio !== 1) {
   meta['retina resolution'] = true;
 }
+// function updateRetina() {
+//   const ratio = wind.meta['retina resolution'] ? pxRatio : 1;
+//   canvas.width = glCanvas.clientWidth * ratio;
+//   canvas.height = glCanvas.clientHeight * ratio;
+//   wind.resize();
+// }
+
+// updateRetina();
+
+
+var ctx = canvas.getContext('2d');
+
+canvas.width  = width;
+canvas.height = height;
 const deltaLong = extent[2] -extent[0];
 const deltaLat = extent[3] -extent[1];
 const longMin = extent[0]
 const latMin = extent[1];
 const NUM_POINTS = vectorData.length;
 
-function toRadians (angle) {
-  return angle * (Math.PI / 180);
-}
 const vy = new Float32Array(NUM_POINTS),
     x = new Float32Array(NUM_POINTS),
     y = new Float32Array(NUM_POINTS),
@@ -47,39 +62,56 @@ for (var i = 0; i < NUM_POINTS; i++) {
     const flatCoordinates = vectorData[i].flatCoordinates_;
     const magnitude = vectorData[i].properties_.Magnitude;
     const direction = vectorData[i].properties_.Direction;
-    x[i] = Math.floor(((flatCoordinates[0] - longMin) / deltaLong ) * width);
-    y[i] = Math.floor(((flatCoordinates[1] - latMin) / deltaLat ) * height);
-    vx[i] = Math.cos(toRadians(direction)) * magnitude *100;
-    vy[i] = Math.sin(toRadians(direction)) * magnitude * 100;
+    //const coords = convert(flatCoordinates[0],flatCoordinates[1], extent)
+    x[i] = ((flatCoordinates[0] - longMin) / deltaLong ) * width;
+    y[i] = ((flatCoordinates[1] - latMin) / deltaLat ) * height;
+    
+    vx[i] = (Math.sin(direction) / magnitude);
+    vy[i] = (Math.cos(direction) / magnitude);
 };
-console.log(vx)
-console.log(vy)
 // const vxScale = vxMax - vxMin;
 // const vyScale = vyMax - vyMin;
 const vxMax = Math.max(...vx);
 const vyMax = Math.max(...vy);
 const vxMin = Math.min(...vx);
 const vyMin = Math.min(...vy);
-const variogram_x = kriging.train(vx, x, y, "linear", 0, 10);
-const variogram_y = kriging.train(vy, x, y, "linear", 0, 10);
+const variogram_x = kriging.train(vx, x, y, "exponential", 0, 100);
+const variogram_y = kriging.train(vy, x, y, "exponential", 0, 100);
 
-const data = new Uint8Array(width*height*4);
+
 for (let y = 0; y < height; y++) {
   for (let x = 0; x < width; x++) {
-      const i = (y * width + x) * 4;
       var vxpredicted = kriging.predict(x, y, variogram_x);
       var vypredicted = kriging.predict(x, y, variogram_y);
-      data[i + 0] = 0
-      data[i + 1] = 0
-      data[i + 2] = Math.floor(255 * (vxpredicted - vxMin) / (vyMax - vyMin));
-      data[i + 3] = Math.floor(255 * (vypredicted - vxMin) / (vyMax - vyMin));
+
+      const r = Math.floor(255 * (vxpredicted - vxMin) / (vxMax - vxMin));
+      const g = Math.floor(255 * (vypredicted - vyMin) / (vyMax - vyMin));
+      // console.log(vxpredicted, vypredicted)
+      ctx.putImageData(new ImageData(new Uint8ClampedArray([r, g, 0, 255]), 1 ,1), x, y);
+      // new Uint8Array()
   }
 }
+const imgData =canvas.toDataURL("image/png");
+const img = document.createElement('img');
+img.src = imgData;
 
 
+// const data = new Uint8Array(width*height*4);
+// for (let y = 0; y < height; y++) {
+//   for (let x = 0; x < width; x++) {
+//       const i = (y * width + x) * 4;
+//       var vxpredicted = kriging.predict(x, y, variogram_x);
+//       var vypredicted = kriging.predict(x, y, variogram_y);
+//       data[i + 0] = Math.floor(255 * (vxpredicted - vxMin) / (vxMax - vxMin));;
+//       data[i + 1] = Math.floor(255 * (vypredicted - vyMin) / (vyMax - vyMin));
+//       data[i + 2] = 0;
+//       data[i + 3] = 255;
+//   }
+// }
+// // console.log(data);
 const windData = {
   // image: new Uint8Array(buffer),
-  image: data,
+  image: img,
   "uMin": vxMin,
   "uMax": vxMax,
   "vMin": vyMin,
